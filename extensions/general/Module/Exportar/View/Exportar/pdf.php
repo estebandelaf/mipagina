a71fff948d7e19f61200fa99993d39eb5dde9d63
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-02-12 10:47:04 -0300

    Título alineado a la derecha

--- extensions/general/Module/Exportar/View/Exportar/pdf.php
+++ extensions/general/Module/Exportar/View/Exportar/pdf.php
@@ -32,10 +32,10 @@ $subtitulo = utf8_decode(Configure::read('page.body.title'));
 $pdf->Ln(6);
 $pdf->SetFont('B', 16);
 $pdf->SetXY(80, 15);
-$pdf->MultiCell(130, 5, $titulo, 0, 'C');
+$pdf->MultiCell(130, 5, $titulo, 0, 'R');
 $pdf->SetFont('B', 10);
 $pdf->SetX(80);
-$pdf->MultiCell(130, 5, $subtitulo, 0, 'C');
+$pdf->MultiCell(130, 5, $subtitulo, 0, 'R');
 $pdf->Ln(15);
 
 // convertir codificación títulos (por si acaso)
