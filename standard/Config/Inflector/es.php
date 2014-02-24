66dcbe71712d61ed30aa159637ac51f9c09fd8c4
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-02-24 20:50:07 -0300

    Ahora se pueden configurar las reglas que se cargan a Inflector según idioma de la aplicación

--- standard/Config/Inflector/es.php
+++ standard/Config/Inflector/es.php
@@ -3,53 +3,47 @@
 /**
  * MiPaGiNa (MP)
  * Copyright (C) 2014 Esteban De La Fuente Rubio (esteban[at]delaf.cl)
- * 
+ *
  * Este programa es software libre: usted puede redistribuirlo y/o
  * modificarlo bajo los términos de la Licencia Pública General GNU
  * publicada por la Fundación para el Software Libre, ya sea la versión
  * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
  * misma.
- * 
+ *
  * Este programa se distribuye con la esperanza de que sea útil, pero
  * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
  * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
  * Consulte los detalles de la Licencia Pública General GNU para obtener
  * una información más detallada.
- * 
+ *
  * Debería haber recibido una copia de la Licencia Pública General GNU
  * junto a este programa.
  * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
  */
 
 /**
- * @file core.php
- * Configuración estándar de las páginas o aplicaciones
+ * @file es.php
+ * Reglas de español para clase Inflector
+ * @author http://joecabezas.tumblr.com/post/572538183/espanolizando-cakephp-mediante-inflections-version
  * @version 2014-02-24
  */
 
-// Errores
-Configure::write('debug', true);
-Configure::write('error.level', E_ALL);
+Inflector::rules('singular', array(
+	'rules' => array (
+		'/bles$/i' => 'ble', // by DeLaF
+		'/([r|d|j|n|l|m|y|z])es$/i' => '\1',
+		'/as$/i' => 'a',
+		'/([ti])a$/i' => '\1a'
+	),
+	'irregular' => array(),
+	'uninflected' => array()
+));
 
-// Tiempo
-Configure::write('time.zone', 'America/Santiago');
-Configure::write('time.format', 'Y-m-d H:i:s');
-
-// Lenguaje de la página
-Configure::write('language', 'es');
-
-// Variables que deberán ser tratadas como globales al renderizar (si
-// es que existen, sino se omitirán)
-Configure::write('page.globals', array());
-
-// Extensiones para las páginas que se desean renderizar
-Configure::write('page.extensions', array('php', 'markdown'));
-
-// Página inicial
-Configure::write('homepage', 'inicio');
-
-// Textos de la página
-Configure::write(
-	'page.footer',
-	'powered by <a href="http://mi.delaf.cl/mipagina">MiPaGiNa</a>'
-);
+Inflector::rules('plural', array(
+	'rules' => array (
+		'/([r|d|j|n|l|m|y|z])$/i' => '\1es',
+		'/a$/i' => '\1as'
+	),
+	'irregular' => array(),
+	'uninflected' => array()
+));
--- standard/Core/Configure.php
+++ standard/Core/Configure.php
@@ -24,7 +24,7 @@
 /**
  * Clase para configurar la aplicación
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2013-04-09
+ * @version 2014-02-24
  */
 class Configure {
 
@@ -33,7 +33,7 @@ class Configure {
 	/**
 	 * Realizar configuración al inicio de la aplicación
 	 * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-	 * @version 2013-04-22
+	 * @version 2014-02-24
 	 */
 	public static function bootstrap() {
 		// Incluir configuraciones para el sitio web
@@ -58,18 +58,8 @@ class Configure {
 			define('TMP', DIR_WEBSITE.DS.'tmp');
 		else
 			define('TMP', sys_get_temp_dir());
-		// cargar inflector con reglas para español
-		// http://joecabezas.tumblr.com/post/572538183/espanolizando-cakephp-mediante-inflections-version
-		Inflector::rules('singular', array(
-			'rules' => array('/([r|d|j|n|l|m|y|z])es$/i' => '\1', '/as$/i' => 'a', '/([ti])a$/i' => '\1a'),
-			'irregular' => array(),
-			'uninflected' => array()
-		));
-		Inflector::rules('plural', array(
-			'rules' => array('/([r|d|j|n|l|m|y|z])$/i' => '\1es','/a$/i' => '\1as'),
-			'irregular' => array(),
-			'uninflected' => array()
-		));
+		// cargar reglas de Inflector para el idioma de la aplicación
+		App::import ('Config/Inflector/'.self::read('language'));
 		// procesar cada ruta (excepto standard) buscando funciones y bootstrap
 		$paths = array_reverse(App::paths());
 		array_shift($paths);
