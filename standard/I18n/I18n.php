056cc40bd2e10829cd000e41af2b131e3c78732e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-03-02 12:15:09 -0300

    Añadido soporte para internacionalización utilizando gettext(). Ver clase I18n para como generar archivos de traducción.

--- standard/I18n/I18n.php
+++ standard/I18n/I18n.php
@@ -24,21 +24,60 @@
 /**
  * Clase para manejar la internacionalización
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2014-03-01
+ * @version 2014-03-02
  */
 final class I18n {
 
+	public static $locale = array ( ///< Mapeo de idioma a locale
+		'es' => 'es_CL.utf8',
+		'en' => 'en_US.utf8',
+	);
+
 	/**
-	 * Función que realiza la traducción de la frase a otro idioma
-	 * @warning Método en desarrollo, no es recomendable usarlo aún
+	 * Función que realiza la traducción de un string a otro idioma.
+	 *
+	 * Plantilla para archivo master.po (para locale en_US.utf8):
+	 * 
+	 *	msgid ""
+	 *	msgstr ""
+	 *	"Project-Id-Version: proyecto en_US master\n"
+	 *	"PO-Revision-Date: 2014-03-02 11:37-0300\n"
+	 *	"Last-Translator: Nombre del traductor <traductor@example.com>\n"
+	 *	"Language-Team: English\n"
+	 *	"Language: en_US\n"
+	 *	"MIME-Version: 1.0\n"
+	 *	"Content-Type: text/plain; charset=UTF-8\n"
+	 *	"Content-Transfer-Encoding: 8bit\n"
+	 *	"Plural-Forms: nplurals=2; plural=(n != 1);\n"
+	 *	
+	 *	msgid "Buscar"
+	 *	msgstr "Search"
+	 *
+	 * Guardar la plantilla en DIR_WEBSITE/Locale/en_US.utf8/LC_MESSAGES/master.po
+	 * Luego ejecutar:
+	 *   $ msgfmt master.po -o master.mo
+	 *
+	 * En caso que se esté creando desde un archivo pot se debe crear el archivo po con:
+	 *   $ msginit --locale=en_US.utf8 --input=master.pot
+	 * Lo anterior creará el archivo en_US.po y luego se usa msgfmt con este archivo
+	 *
+	 * Cuando se crean o modifican los directorios en DIR_WEBSITE/Locale se
+	 * debe reiniciar el servicio Apache (¿?)
+	 *
 	 * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-	 * @version 2014-03-01
+	 * @version 2014-03-02
 	 */
-	public static function translate ($string, $language = null) {
-		if (!$language) {
-			$language = Session::read('config.language');
+	public static function translate ($string, $locale = null, $domain = 'master', $encoding = 'UTF-8') {
+		if (!$locale) {
+			$locale = Session::read('config.language');
 		}
-		return $string;
+		if (!strpos($locale, '_')) $locale = self::$locale[$locale];
+		putenv("LANG=".$locale);
+		setlocale( LC_MESSAGES, $locale);
+		bindtextdomain($domain, DIR_WEBSITE.'/Locale');
+		textdomain($domain);
+		bind_textdomain_codeset($domain, $encoding);
+		return gettext($string);
 	}
 
 }
